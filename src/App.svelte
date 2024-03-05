181b47653386c80a470355f321a5fd254bd30bcd
Danny <danniharany@gmail.com>
2024-03-05 14:10:53 -0500

    Testing changes

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
   import Banner from './lib/Banner.svelte';
   import Carousel from './lib/Carousel.svelte';
   import ProgressContainer from './lib/ProgressContainer.svelte';
-  import IdeaContainer from './lib/IdeaContainer.svelte';
+  import IdeaSection from './lib/IdeaSection.svelte';
 
   alert("This website is not complete yet, the content is simply placeholder");
 </script>
@@ -57,7 +57,7 @@
       <div class="project_containers">
         <div class="update_containers">
           <div class="idea_container">
-            <IdeaContainer/>
+            <IdeaSection/>
           </div>
           <div class="progress_container">
             <ProgressContainer />
